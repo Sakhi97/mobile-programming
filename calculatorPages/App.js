62d0b1324beb42e20737a9aff1691e0c0023866b
Sakhi <78950775+Sakhi97@users.noreply.github.com>
2023-09-06 15:48:04 +0300

    calculator with pages

---
+++ calculatorPages/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Calculator from './components/Calculator';
+import History from './components/History';
+
+const Stack = createNativeStackNavigator ();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Calculator">
+        <Stack.Screen name="Calculator" component={Calculator} />
+        <Stack.Screen name="History" component={History} options={{
+          headerTitle: 'History',
+        }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
